1ff4e4f484d0744d110bf92963bf61bcbda0ce67
Tom Christensen <tom.m.christensen@gmail.com>
2013-02-22 14:03:50 +0100

    testklasse

---
+++ src/XML_handin3/StaxTest.java
@@ -0,0 +1,18 @@
+package XML_handin3;
+
+public class StaxTest {
+
+	/**
+	 * @param args
+	 */
+	  public static void main(String[] args) {
+		    StaxWriter configFile = new StaxWriter();
+		    configFile.setFile("config2.xml");
+		    try {
+		      configFile.saveConfig();
+		    } catch (Exception e) {
+		      e.printStackTrace();
+		    }
+		  }
+
+}
