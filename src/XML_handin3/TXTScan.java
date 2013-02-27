c977840580b8c6289be009b10dd785e508e37eb8
Jonas Jørgensen <jelb@itu,dk>
2013-02-27 09:44:56 +0100

    Ny REGEX til løsning af komma problem

--- src/XML_handin3/TXTScan.java
+++ src/XML_handin3/TXTScan.java
@@ -1,30 +1,55 @@
 package XML_handin3;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 
 public class TXTScan {
-private Scanner scan;
-private String[] arr, nyArr;
- public TXTScan(String filePath) {
-  try {
-   scan = new Scanner(new File(filePath)).useDelimiter("[\\n]");
-  } catch (FileNotFoundException e) {
-   // TODO Auto-generated catch block
-   e.printStackTrace();
-  }
- }
+	private Scanner scan;
+	private String[] nyArr;
+	
+	public TXTScan(String filePath) {
+		try {
+			scan = new Scanner(new File(filePath)).useDelimiter("[\\n]");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
  
-  public String[] scanning(int tal){
-  nyArr = scan.next().split(",");
-  while(nyArr.length != tal){nyArr = scan.next().split(","); System.out.println(nyArr[0]);}
-  nyArr[nyArr.length -1] = nyArr[nyArr.length -1].trim();
-  return nyArr;
- }
+	public String[] scanning(int tal){
+		String[] temp = scan.next().split(",(?! |[a-zA-ZæÆøØåÅ])");
+		temp[temp.length -1] = temp[temp.length-1].trim();
+		if(temp.length == 33){
+			nyArr = new String[]{
+					temp[0], temp[1], temp[2], temp[6], temp[7], 
+					temp[8], temp[9], temp[10], temp[11], temp[12], 
+					temp[13], temp[14],temp[17], temp[18], temp[26]};
+		} else {
+			nyArr = temp;
+		}
+		return nyArr;
+	}
 
- public boolean hasNext() {
-   return scan.hasNext();
- }
+	public String[] titleScanning(int tal){
+		String[] temp = scan.next().split("( |,)");
+		temp[temp.length -1] = temp[temp.length-1].trim();
+		if(temp.length == 33){
+			nyArr = new String[]{
+					temp[0], temp[1], temp[2], temp[6], temp[7], 
+					temp[8], temp[9], temp[10], temp[11], temp[12], 
+					temp[13], temp[14],temp[17], temp[18], temp[26]};
+		} else {
+			nyArr = temp;
+		}
+		return nyArr;
+	}
+	
+	public boolean hasNext() {
+		return scan.hasNext();
+	}
+	
+	public void closeFile() {
+		scan.close();
+	}
 }
