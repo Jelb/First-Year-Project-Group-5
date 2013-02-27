622fda87ad21761e4ad4c8bdd44cd1c014c2af9c
Tom Christensen <tom.m.christensen@gmail.com>
2013-02-27 11:47:23 +0100

    Startet på GraphBuilder

--- src/HandIn4/GraphBuilder.java
+++ src/HandIn4/GraphBuilder.java
@@ -1,5 +1,27 @@
 package HandIn4;
 
-public class GraphBuilder {
+public abstract class GraphBuilder {
 
+	private static void main(String[] args) {
+		init();
+	}
+	
+	private static void init() {
+		createNodes();
+		createEdges();
+	}
+	
+	private static void createNodes() {
+		// skriv en Node XML parser
+		// byg en node
+		// kald Node.inserNode() med den
+		
+		Node node = new Node(692067.66450, 6049914.43018, 675902);  // hardcodet et eksempel
+		Node.insertNode(node);
+	}
+	
+	// eller noget...
+	private static void createEdges() {
+		
+	}
 }
