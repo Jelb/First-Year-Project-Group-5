96a9f4846a6b6781d064e8e7e73245b63ebaab33
Tom Christensen <tom.m.christensen@gmail.com>
2013-03-13 11:08:07 +0100

    Moar classes!

---
+++ src/Part1/Graph.java
@@ -0,0 +1,44 @@
+package Part1;
+
+import java.util.LinkedList;
+
+// Based on code on p. 611 in Algorithms 4. ed. Sedgewick, Wayne
+public class Graph {
+	private final int V;	// number of nodes
+	private int E;			// number of edges
+	private LinkedList<Edge>[] adj; // array of adjacency lists
+	
+	public Graph(int V) {
+		this.V = V;
+		this.E = 0;
+		adj = (LinkedList<Edge>[])new LinkedList[V];
+		// creates V linked lists, one for each node
+		for (int v = 0; v < V; v++) {
+			adj[v] = new LinkedList<Edge>();
+		}
+		
+	}
+	
+	public int getV() { return V; }
+	public int getE() { return E; }
+	
+	// adds an edge to the nodes that the edge connects
+	public void addEdge(Edge e) {
+		int v = e.either(), w = e.other(v);
+		adj[v].add(e);
+		adj[w].add(e);
+		E++;
+	}
+	
+	// returns adjacency list for the given node
+	public Iterable<Edge> adj(int v) { return adj[v]; }
+
+	// returns linked list all edges in the graph
+	public Iterable<Edge> edges() {
+		LinkedList<Edge> list = new LinkedList<Edge>();
+		for (int v = 0; v < V; v++)
+			for (Edge e : adj[v])
+				if (e.other(v) > v) list.add(e);
+		return list;
+	}
+}
---
+++ src/Part1/Node.java
@@ -0,0 +1,37 @@
+package Part1;
+
+public class Node {
+	
+	private double xCord;
+	private double yCord;
+	private int kdvID;
+	private static Node[] nodeArray = new Node[675901];
+	
+	public Node(double xCord, double yCord, int kdvID) {
+		this.xCord = xCord;
+		this.yCord = yCord;
+		this.kdvID = kdvID;
+	}
+	
+	public double getXCord(){
+		return xCord;
+	}
+	
+	public double getYCord(){
+		return yCord;
+	}
+	
+	public int getKdvID(){
+		return kdvID;
+	}
+	
+	public static void insertNode(Node node){
+		int i = node.getKdvID();
+		nodeArray[i] = node;
+	}
+	
+	public static Node getNode(int index) {
+		return nodeArray[index];
+	}
+
+}
