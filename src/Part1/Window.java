23b4bcb41cd0d309028f2062e4674a3360b98703
Christopher Bjerre <chbjerre@gmail.com>
2013-03-13 11:26:54 +0100

    Start on Window class

--- src/Part1/Window.java
+++ src/Part1/Window.java
@@ -1,5 +1,20 @@
 package Part1;
 
-public class Window {
+/*
+ * Window class is the GUI of our program, including drawing of the map.
+ */ 
 
+public class Window {
+	
+	private RoadSegment[] segmentArray;
+
+	public Window(){
+		
+	}
 }
+
+//Can draw lines from RoadSegment[] 
+//where RoadSegment includes from(x,y) to(x,y) and colour
+//Pixel vs coordninate problem
+//Centre x,y coordinate
+// Scaling/rezising
