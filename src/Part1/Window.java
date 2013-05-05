466f29d95367f8be771ece62ced62b78ede5f9b9
Mads Engesgaard Jacobsen <madsengesgaard@gmail.com>
2013-05-05 13:15:08 +0200

    Nu bliver search boxen opdateret når en ny søgning udføres

--- src/Part1/Window.java
+++ src/Part1/Window.java
@@ -353,7 +353,6 @@ public class Window extends JFrame {
 			}
 			createSearchBox(setArray,x,y,fromBool);
 		}
-		updateMap();
 	}
 	
 	/**
@@ -366,7 +365,7 @@ public class Window extends JFrame {
 	 */
 	private void createSearchBox(String[] array, int x, int y,boolean fromBool){
 		Window.fromBool = fromBool;
-		searchResultBox = null;
+		screen.remove(searchResultBox);
 		searchResultBox = new JComboBox(array);
 		searchResultBox.setBounds(x,y ,200,25);	
 		searchResultBox.addActionListener(new ActionListener(){ 
